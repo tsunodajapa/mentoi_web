e6749916554fc7b6d7196ebef0e52b41fffb86b1
tsunodagabriel <gabriel.tsunoda@vitta.me>
2021-07-12 23:32:33 -0300

    chore: portuguese errors

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -16,18 +16,19 @@ const AboutUs = () => {
           <h2>Sobre o MENTOI</h2>
           <p>
             Mentoi é uma plataforma educacional onde é possível realizar
-            perguntas sobre diferentes áreas, que serão respondidas professores
-            ou profissionais da área, garantindo responsabilidade nas respostas,
-            sem Fake News ou teorias da conspiração. Contribuindo, também, para
-            uma divulgação do conhecimento científico.
+            perguntas sobre diferentes áreas, que serão respondidas por
+            professores ou profissionais da área, garantindo responsabilidade
+            nas respostas, sem Fake News ou teorias da conspiração.
+            Contribuindo, também, para uma divulgação do conhecimento
+            científico.
           </p>
 
           <h2>Por que existimos?</h2>
           <p>
             Nosso objetivo principal é combater o ideal anticientífico que tem
             abalado nossa sociedade nos últimos anos, com teorias da conspiração
-            e fakenews, sendo um espaço de conversa direta entre usuários e
-            especialistas da área. Assim, nós ajudamos os estudantes, a terem
+            e Fake News, sendo um espaço de conversa direta entre usuários e
+            especialistas da área. Assim, nós ajudaremos os estudantes, a terem
             respostas didáticas e com credibilidade, criando um espaço
             confortável para a construção do conhecimento.
           </p>
